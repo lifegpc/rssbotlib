58e2f9a91e7f2c25f26f18a322e01e3c8034f548
lifegpc <g1710431395@gmail.com>
2021-01-15 08:49:18 +0800

    update

--- src/build.sh
+++ src/build.sh
@@ -1,6 +1,7 @@
 cd main
 includePath=-I../thirdparty/ffmpeg
-g++ $includePath -shared -c getdur.cpp
+GPP=g++ -fPIC -shared
+$GPP $includePath -c getdur.cpp
 TEMP_PKG_CONFIG_PATH=$PKG_CONFIG_PATH
 export PKG_CONFIG_PATH=pkgconfig/:$PKG_CONFIG_PATH
 libavformat=$(pkg-config --libs libavformat)
@@ -9,4 +10,4 @@ case "$OSTYPE" in
     linux*) OUTPUT_DLL=rssbot.so;;
     *) OUTPUT_DLL=rssbot.dll;;
 esac
-g++ -shared -o $OUTPUT_DLL getdur.o $libavformat
+$GPP -shared -o $OUTPUT_DLL getdur.o $libavformat
