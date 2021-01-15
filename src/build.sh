3f3fd1fa81d2f764ce86286d02608fdb82cd5db5
lifegpc <g1710431395@gmail.com>
2021-01-15 08:38:32 +0800

    choose ext name by OSTYPE

--- src/build.sh
+++ src/build.sh
@@ -5,4 +5,8 @@ TEMP_PKG_CONFIG_PATH=$PKG_CONFIG_PATH
 export PKG_CONFIG_PATH=pkgconfig/:$PKG_CONFIG_PATH
 libavformat=$(pkg-config --libs libavformat)
 export PKG_CONFIG_PATH=$TEMP_PKG_CONFIG_PATH
-g++ -shared -o rssbot.dll getdur.o $libavformat
+case "$OSTYPE" in 
+    linux*) OUTPUT_DLL=rssbot.so;;
+    *) OUTPUT_DLL=rssbot.dll;;
+esac
+g++ -shared -o $OUTPUT_DLL getdur.o $libavformat
