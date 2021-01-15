a3cc18c4fc466637ed7e76802a40b658b1903592
lifegpc <g1710431395@gmail.com>
2021-01-15 09:21:57 +0800

    fix bug

--- src/build_ffmpeg.sh
+++ src/build_ffmpeg.sh
@@ -1,4 +1,10 @@
 cd thirdparty/ffmpeg
 make install
+cd ../../main/
+if [test -d pkgconfig]
+then
+    rm -rfv pkgconfig
+fi
+cd ../thirdparty/ffmpeg
 mv -f pkgconfig ../../main/
 mv -f *.a ../../main/
