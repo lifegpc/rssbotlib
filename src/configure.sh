94b8a8320f768504414554e42e92f2752a268f03
lifegpc <g1710431395@gmail.com>
2021-01-15 10:32:54 +0800

    add muxer

--- src/configure.sh
+++ src/configure.sh
@@ -1,2 +1,2 @@
 cd thirdparty/ffmpeg
-./configure --enable-gpl --enable-version3 --libdir=./ --disable-programs --disable-encoders --disable-asm --disable-decoders --disable-hwaccels --disable-muxers --disable-bsfs --disable-devices --disable-filters --enable-gnutls
+./configure --enable-gpl --enable-version3 --libdir=./ --disable-programs --disable-encoders --disable-asm --disable-decoders --disable-hwaccels --disable-muxers --disable-bsfs --disable-devices --disable-filters --enable-gnutls --enable-muxer=mp4 --enable-muxer=matroska
