9a5b0d0a3de2fbac2fba819a65b389154f605da3
lifegpc <g1710431395@gmail.com>
2021-01-17 14:36:38 +0800

    upload 7z compress to workflow

--- github_action_workflow_ubuntu.sh
+++ github_action_workflow_ubuntu.sh
@@ -8,3 +8,5 @@ if [ $? -ne 0 ]; then exit 1; fi
 if [ $? -ne 0 ]; then exit 1; fi
 ./build.sh
 if [ $? -ne 0 ]; then exit 1; fi
+7z a -stm1 -y -mx9 ../release.7z main/rssbot.so
+if [ $? -ne 0 ]; then exit 1; fi
