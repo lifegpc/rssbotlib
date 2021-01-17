960d25b05a8df063fc45cf448080581b8f4fde9c
lifegpc <g1710431395@gmail.com>
2021-01-17 17:30:27 +0800

    update lib

--- github_action_workflow_windows.sh
+++ github_action_workflow_windows.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
-pacman --noconfirm --needed -S mingw64/mingw-w64-x86_64-gcc
-if [ $? -ne 0 ]; then exit 1; fi
-pacman --noconfirm --needed -S mingw64/mingw-w64-x86_64-gnutls
-if [ $? -ne 0 ]; then exit 1; fi
-pacman --noconfirm --needed -S msys/p7zip
-if [ $? -ne 0 ]; then exit 1; fi
 cd src
 ./configure.sh
 if [ $? -ne 0 ]; then exit 1; fi
